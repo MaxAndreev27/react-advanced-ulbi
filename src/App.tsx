2390427767e83ad1ce0614e46d9010a38b0b8729
MaxAndreev27 <maxandreev27@gmail.com>
2025-01-08 18:38:15 +0200

    Theme toggle final fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,10 @@ import {Link, Route, Routes} from 'react-router-dom';
 import './styles/index.scss';
 import {AboutAsync} from "./pages/about/About.async";
 import {MainAsync} from "./pages/main/Main.async";
-import {Theme, ThemeContext} from "./theme/ThemeContext";
+import {useTheme} from "./theme/useTheme";
 
 const App = () => {
-    //const {theme, toggleTheme} = useTheme();
-
-    const {theme, setTheme} = useContext(ThemeContext);
-
-    // const [theme, setTheme] = useState('light');
-    //
-    const toggleTheme = () => {
-        setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK);
-    }
+    const {theme, toggleTheme} = useTheme();
 
     return (
         <div className={`app ${theme}`}>
