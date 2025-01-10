7123d26ee12640eaa450b7067cbe7c922ac12dda
MaxAndreev27 <maxandreev27@gmail.com>
2025-01-10 19:05:21 +0200

    Add Sidebar, change position ThemeSwitcher from navbar to sidebar

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -15,7 +15,6 @@ declare module '*.svg' {
     export default SVG;
 
     // import React from 'react';
-    //
     // const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
     // export default SVG;
 }
