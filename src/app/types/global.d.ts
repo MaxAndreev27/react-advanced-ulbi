ca76cb9a322ac28a6e9eee561696c434867ec8aa
MaxAndreev27 <maxandreev27@gmail.com>
2025-01-10 21:58:43 +0200

    Add i18n localisation

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -13,8 +13,6 @@ declare module '*.svg' {
     import { FC, SVGProps } from "react";
     const SVG: FC<SVGProps<SVGElement>>;
     export default SVG;
-
-    // import React from 'react';
-    // const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
-    // export default SVG;
 }
+
+declare const __IS_DEV__: boolean;
--- src/pages/main/ui/Main.tsx
+++ src/pages/main/ui/Main.tsx
@@ -1,8 +1,11 @@
+import {useTranslation} from "react-i18next";
 
 const Main = () => {
+    const { t } = useTranslation('main');
+
     return(
         <div>
-            <h1>Main</h1>
+            <h1>{t('Main page content')}</h1>
         </div>
     )
 }
