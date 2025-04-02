246ceaa7ca7c21ae24739c3d50a53f98b3cd96ed
MaxAndreev27 <maxandreev27@gmail.com>
2025-04-02 21:53:32 +0300

    Update storybook config (move webpack.config.ts to main.ts). Update package @reduxjs/toolkit 2.5.1->2.6.1, problems with tests (updateProfileData.test.ts, fetchNextArticlesPage.test.ts), temporary remove this tests

--- config/storybook/main.ts
+++ config/storybook/main.ts
@@ -1,5 +1,9 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
 import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin';
+import { BuildPaths } from '../build/types/config';
+import path from 'path';
+import { DefinePlugin, RuleSetRule } from 'webpack';
+import { buildCssLoader } from '../build/loaders/buildCssLoader';
 
 const config: StorybookConfig = {
     framework: {
@@ -19,6 +23,43 @@ const config: StorybookConfig = {
         'storybook-addon-mock',
     ],
     webpackFinal: async (config) => {
+        const paths: BuildPaths = {
+            build: '',
+            html: '',
+            entry: '',
+            src: path.resolve(__dirname, '..', '..', 'src'),
+            locales: '',
+            buildLocales: '',
+            favicon: '',
+            buildFavicon: '',
+        };
+        config!.resolve!.modules!.push(paths.src);
+        config!.resolve!.extensions!.push('.ts', '.tsx');
+
+        // eslint-disable-next-line no-param-reassign
+        //@ts-ignore
+        config!.module!.rules = config!.module!.rules!.map((rule: RuleSetRule) => {
+            if (/svg/.test(rule.test as string)) {
+                return { ...rule, exclude: /\.svg$/i };
+            }
+
+            return rule;
+        });
+
+        config!.module!.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        });
+        config!.module!.rules.push(buildCssLoader(true));
+
+        config!.plugins!.push(
+            new DefinePlugin({
+                __IS_DEV__: JSON.stringify(true),
+                __API__: JSON.stringify('https://testapi.com'),
+                __PROJECT__: JSON.stringify('storybook'),
+            }),
+        );
+
         if (config.resolve) {
             config.resolve.plugins = [
                 ...(config.resolve.plugins || []),
