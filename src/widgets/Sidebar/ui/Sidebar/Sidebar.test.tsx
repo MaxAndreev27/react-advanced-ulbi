35ebb1a143d750406c8b2436dcd3bbeb6255e19b
MaxAndreev27 <maxandreev27@gmail.com>
2025-04-17 21:53:42 +0300

    Fix prettier

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -14,8 +14,8 @@ describe('Sidebar', () => {
         const toggleBtn = screen.getByTestId('sidebar-toggle');
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
         fireEvent.click(toggleBtn);
-        // expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
-        expect(screen.getByTestId('sidebar')).toHaveClass('collapsedRedesigned');
+        expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
+        // expect(screen.getByTestId('sidebar')).toHaveClass('collapsedRedesigned');
         // screen.debug();
     });
 });
