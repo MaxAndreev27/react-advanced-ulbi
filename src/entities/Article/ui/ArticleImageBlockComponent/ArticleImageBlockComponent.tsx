0dbad39e6b683d99bdbc01c08acd468500ca1b7f
MaxAndreev27 <maxandreev27@gmail.com>
2025-04-18 19:25:16 +0300

    Redesign ArticleDetailsPage content blocks

--- src/entities/Article/ui/ArticleImageBlockComponent/ArticleImageBlockComponent.tsx
+++ src/entities/Article/ui/ArticleImageBlockComponent/ArticleImageBlockComponent.tsx
@@ -1,8 +1,11 @@
-import { classNames } from '@/shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import { Text as TextDeprecated, TextAlign } from '@/shared/ui/deprecated/Text';
 import { Text } from '@/shared/ui/redesigned/Text';
 import cls from './ArticleImageBlockComponent.module.scss';
 import { ArticleImageBlock } from '../../model/types/article';
+import { ToggleFeatures } from '@/shared/lib/features';
 
 interface ArticleImageBlockComponentProps {
     className?: string;
@@ -11,11 +14,18 @@ interface ArticleImageBlockComponentProps {
 
 export const ArticleImageBlockComponent = memo((props: ArticleImageBlockComponentProps) => {
     const { className, block } = props;
+    const { t } = useTranslation();
 
     return (
         <div className={classNames(cls.ArticleImageBlockComponent, {}, [className])}>
             <img src={block.src} alt={block.title} className={cls.img} />
-            {block.title && <Text text={block.title} align={'center'} />}
+            {block.title && (
+                <ToggleFeatures
+                    feature="isAppRedesigned"
+                    on={<Text text={block.title} align="center" />}
+                    off={<TextDeprecated text={block.title} align={TextAlign.CENTER} />}
+                />
+            )}
         </div>
     );
 });
